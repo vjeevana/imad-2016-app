9ff64bb6423738298ad0d7959bdf1c6f1d57e327
vjeevana <vjeeevana@gmail.com>
2016-10-20 20:40:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/Article-one', function (req, res){
+res.send("Article 1 requested will be served here.");
+});
+app.get('/Article-two', function (req, res){
+res.send("Article 2 requested will be served here.");
+});
+app.get('/Article-three', function (req, res){
+res.send("Article 3 requested will be served here.");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
