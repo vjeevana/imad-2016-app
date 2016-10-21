b7e00425a7532bf3453510dd334341061aadfa2e
vjeevana <vjeeevana@gmail.com>
2016-10-21 19:48:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML="new value"
+var element = document.getElementById(`main-text`);
+element.innerHTML="new value";
