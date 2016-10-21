f36b6be66b9d64c4804364b9a1e865052bb6f3ac
vjeevana <vjeeevana@gmail.com>
2016-10-21 22:11:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,15 @@
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function () {
     
 
     var names = ['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0; i< names.length; i++){
+    var list = '';
+    for (var i=0; i< names.length; i++) {
         list == '<li>' + names[i] + '</li>';
         
     }
-    var ul= document.getElementById('namelist');
+    var ul = document.getElementById('namelist');
     ul.innerHTML = list;
 };
