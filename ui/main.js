8d0ec09dae6cfe98b84e38b14c9b35af19b10c6b
vjeevana <vjeeevana@gmail.com>
2016-10-21 20:03:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,4 @@ console.log('Loaded!');
 var element = document.getElementById(`main-text`);
 element.innerHTML = `new value`;
 //move the image
-var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft = '100px';
-};
+
