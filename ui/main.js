ec5045a491e760545b8f0e3979264a48d00d35f4
vjeevana <vjeeevana@gmail.com>
2016-10-21 20:17:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ element.innerHTML = `new value`;
 //move the image
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function moveRight (){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = margnLeft + 'px';
+function moveRight () {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
    var interval = setInterval(moveRight, 50);
